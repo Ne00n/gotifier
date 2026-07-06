9eb80372a588f8e8f95881d523498668ca8f60f7
Ne00n <neo@tiefkuehler-madness.me>
2026-07-06 12:48:45 +0200

    update cli

--- cli.py
+++ cli.py
@@ -10,9 +10,18 @@ with open(f"{path}/alerts.json") as handle: alerts = json.loads(handle.read())
 if len(sys.argv) == 1:
     print(options)
 elif sys.argv[1] == "add":
-    timestamp = int(time.mktime(datetime.datetime.strptime(sys.argv[2], "%d.%m.%Y %H:%M").timetuple()))
+    priority, repeat = 10, 0
+    for index, param in enumerate(sys.argv):
+        if param == "--date":
+            timestamp = int(time.mktime(datetime.datetime.strptime(sys.argv[index +1], "%d.%m.%Y %H:%M").timetuple()))
+        if param == "--message":
+            message = sys.argv[index +1]
+        if param == "--priority":
+            priority = int(sys.argv[index +1])
+        if param == "--repeat":
+            repeat = int(sys.argv[index +1])
     id = ''.join(secrets.choice(string.ascii_letters) for _ in range(6))
-    alerts[id] = {"timestamp":timestamp,"message":sys.argv[3]}
+    alerts[id] = {"timestamp":timestamp,"message":message,"priority": priority,"repeat":repeat,"runs":0}
     with open(f"{path}/alerts.json", 'w') as f: json.dump(alerts, f, indent=4)
 elif sys.argv[1] == "list":
     for id,details in alerts.items():
